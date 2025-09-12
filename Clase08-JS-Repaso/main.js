7696f6b7ba6b4605158c6181b3847678140644a8
AgustinFrich <agustinfrich@gmail.com>
2025-09-12 20:15:27 -0300

    asdasdasd

--- Clase08-JS-Repaso/main.js
+++ Clase08-JS-Repaso/main.js
@@ -1,13 +1,20 @@
 const KEY = "VOeL5y635MqjiO3PyXajORdkomN8l1F9h19VtaLH";
+const FECHA_MINIMA = "2025-06-16";
+const FECHA_MAXIMA = "HOY!!";
 
 const tituloElement = document.getElementById("titulo");
 const descElement = document.getElementById("desc");
 const imagenElement = document.getElementById("imagen");
 const cargandoElement = document.getElementById("cargando");
+const fechaElement = document.getElementById("fecha");
 
-async function traerDatosDeHoy() {
+async function traerDatos(date = null) {
   // Hacer la petición a la API
-  const apiUrl = "https://api.nasa.gov/planetary/apod?api_key=" + KEY;
+  let apiUrl = "https://api.nasa.gov/planetary/apod?api_key=" + KEY;
+
+  if (date) {
+    apiUrl += "&date=" + date;
+  }
 
   const resultado = await fetch(apiUrl);
 
@@ -31,4 +38,27 @@ async function traerDatosDeHoy() {
   imagenElement.src = urlImagen;
 }
 
-traerDatosDeHoy();
+fechaElement.onchange = async (event) => {
+  console.log(event.target.value);
+
+  traerDatos(event.target.value);
+};
+// Alternativa para escuchar eventos
+// fechaElement.addEventListener("change", (event) => {
+// })
+
+// De arranque, muestra lo de hoy.
+traerDatos();
+
+function obtenerDiaDeHoy() {
+  const hoy = new Date();
+  // const anio = hoy.getFullYear();
+  // const mes = hoy.getMonth();
+  // const dia = hoy.getDate();
+
+  // console.log(`${anio}-${mes + 1}-${dia}`);
+
+  console.log(hoy.toISOString());
+}
+
+obtenerDiaDeHoy();
--- Clase08-JS-Repaso/objetivo.md
+++ Clase08-JS-Repaso/objetivo.md
@@ -3,18 +3,20 @@ Hacer una página que consuma los datos de la api de la NASA APOD (astronomy pic
 
 # Paso a paso:
 1. HTML
-    - Layout.
-    - El input.
-    - Imagen.
-    - Textos.
+    - Layout. ♥
+    - El input. ♥
+    - Imagen. ♥
+    - Textos. ♥
     - Spinner.
 2. CSS
-    - Ordenar los elementos.
-    - Que entren en pantalla.
+    - Ordenar los elementos. ♥
+    - Que entren en pantalla. ♥
     - Que se adapte a mobile.
     - Opcional: modificar el input de date.
 3. JS
-    - Cuando cargue la página, que traiga los datos de hoy.
-    - Cuando se cambie la date, que traiga los datos de ese día.
+    - Cuando cargue la página, que traiga los datos de hoy. ♥
+    - Cuando se cambie la date, que traiga los datos de ese día. ♥
+        - Obtener qué hay en el input ♥
+        - Captar el evento de cuando cambia ♥
     - Limitar la fecha para que no sea futura o muy pasado.
     - Agregar un spinner (HTML y CSS).
