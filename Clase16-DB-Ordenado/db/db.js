62eacb635ffed2bc35a4bdced761c6ab29abe936
AgustinFrich <agustinfrich@gmail.com>
2025-10-13 22:32:07 -0300

    asdasdasd

--- Clase16-DB-Ordenado/db/db.js
+++ Clase16-DB-Ordenado/db/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require("sequelize");
 const stringDb = process.env.STRING_DB;
 const sequelize = new Sequelize(stringDb);
 
-// sequelize.sync({ force: true });
+sequelize.sync({ alter: true });
 
 // require importa, module.exports exporta
 module.exports = sequelize;
