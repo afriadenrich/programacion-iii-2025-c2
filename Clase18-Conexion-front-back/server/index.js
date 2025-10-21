c0ba959775b4715382211d7ececfba3a7ee0e348
AgustinFrich <agustinfrich@gmail.com>
2025-10-20 21:34:44 -0300

    comentarios y ejemplos

--- Clase18-Conexion-front-back/server/index.js
+++ Clase18-Conexion-front-back/server/index.js
@@ -3,10 +3,10 @@ const app = express();
 const routerAuto = require("./routes/auto.routes");
 const cors = require("cors");
 
-app.use(express.json());
+app.use(express.json()); // Requiere que en los encabezados se solicitud esté: Content-Type: application/json
 app.use(
   cors({
-    origin: process.env.ORIGIN,
+    origin: process.env.ORIGIN, //¿Quién o quienes me pueden hacer peticiones desde navegadores? Sin navegador, no hace nada
   })
 );
 
