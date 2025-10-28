dce74b88db2352143dae27688adcbc10fe8ec73b
AgustinFrich <agustinfrich@gmail.com>
2025-10-27 21:50:10 -0300

    EJS

--- Clase20-EJS/index.js
+++ Clase20-EJS/index.js
@@ -9,18 +9,56 @@ app.set("view engine", "ejs");
 // Setteo la carpeta donde van a ir mis vistas
 app.set("views", "./views");
 
+// Setteo que la carpeta images (u otra/s carpeta/s) sea ESTÁTICA
+app.use("/public", express.static("images"));
+
+// Un poco más pro:
+const path = require("path");
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.use("/assets", express.static(path.join(__dirname, "assets")));
+
+app.use(express.json());
+
+// RUTAS -------------------------------
+
 app.get("/", (req, res) => {
   // Renderizo mi vista index.ejs
   //   res.send("<h1>Hola</h1>");
-  res.render("index");
+  res.render("layout", { path: "index" });
 });
 
 app.get("/home", (req, res) => {
   res.render("home");
 });
 
-app.get("/autos", (req, res) => {
-  res.render("autos");
+app.get("/autos", async (req, res) => {
+  // cosnt autos = await Auto.findAll();
+  const autos = [
+    {
+      marca: "UTN",
+      precio: 100000,
+    },
+    {
+      marca: "Ferrari",
+      precio: 3,
+    },
+    {
+      marca: "Marca",
+      precio: 98099023,
+    },
+  ];
+  // 2do parámetro, variables a incrustar
+  res.render("autos", { autos: autos });
+});
+
+app.get("/complejo", (req, res) => {
+  res.render("complejo");
+});
+app.get("/images/158.png", (req, res) => {
+  res.send("No devuelve la imagen");
+});
+app.get("/158.png", (req, res) => {
+  res.send("No devuelve la imagen");
 });
 
 app.listen(3000, () => {
